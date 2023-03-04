d7174583cc0b3a9d3a3167f018c2b25c33c847ca
NKT <49834917+ADRNV@users.noreply.github.com>
2023-03-04 22:59:54 +0300

    Add infrastructure

---
+++ src/Server/Nocturne/Nocturne.Infrastructure/Securiry/UsersContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Nocturne.Infrastructure.Securiry
+{
+    public class UsersContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ src/Server/Nocturne/Nocturne/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Nocturne.Infrastructure.Securiry;
+
+namespace Nocturne
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public void Configure(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<UsersContext>(options =>
+            {
+                options.UseSqlServer(_configuration.GetConnectionString("UserIdentity"));
+            })
+                .AddDefaultIdentity<IdentityUser>(options =>
+                {
+
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 8;
+                    options.SignIn.RequireConfirmedEmail = false;
+
+                }).AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<UsersContext>();
+        }
+    }
+}
