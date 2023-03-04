d4ca85b36803b6850206f7d7300e7ac725389ad7
NKT <49834917+ADRNV@users.noreply.github.com>
2023-03-04 18:39:47 +0300

    Add core layer

---
+++ src/Server/Nocturne/Nocturne.Core/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace Nocturne.Core.Models
+{
+    public class Message
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+
+        public string From { get; set; }
+
+        public string Content { get; set; }
+    }
+}
---
+++ src/Server/Nocturne/Nocturne.Core/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Nocturne.Core.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<long> Insert(T person);
+
+        Task<bool> Update(T person);
+
+        Task<bool> Delete(T person);
+
+        Task<IEnumerable<T>> Get();
+
+        Task<T> Get(int id);
+    }
+}
