801359e97d8e38bf76e9ba772879bd524f3771b3
NKT <49834917+ADRNV@users.noreply.github.com>
2023-03-26 20:04:27 +0300

    Prepare to tests

---
+++ src/Server/Nocturne/Nocturne.InfrastructureTests1/Helpers/DependencyHelper.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Nocturne.Infrastructure.Caching;
+using Nocturne.Infrastructure.Security.Entities;
+using AutoFixture;
+using Nocturne.Infrastructure.Messaging;
+using Nocturne.Core.Managers;
+using System.Reflection;
+using Nocturne.Infrastructure.Security.MappersConfiguration;
+
+namespace Nocturne.InfrastructureTests.Helpers
+{
+    internal static class DependencyHelper
+    {
+        public static IServiceProvider Services { get; }
+
+        private static Mock<IRedisCacheRepository<Connection>> _redisCacheRepositoryMock { get; }
+
+        private static Mock<UserManager<User>> _userManagerMock { get; }
+
+        private static ConnectionsManager<User> _connectionsManager { get; set; }
+
+        public static IEnumerable<User> UsersContextMock { get; set; } = new Fixture()
+            .Build<User>()
+            .Without(u => u.UserGroups)
+            .With(u => u.UserName)
+            .CreateMany();
+
+        static DependencyHelper()
+        {
+            _redisCacheRepositoryMock = new Mock<IRedisCacheRepository<Connection>>();
+
+            _redisCacheRepositoryMock.Setup(r => r.Insert(It.IsAny<Connection>()))
+                .ReturnsAsync(() =>
+                Guid
+                .NewGuid()
+                .ToString());
+
+           _redisCacheRepositoryMock.Setup(r => r.Delete(It.IsAny<Connection>()))
+                .Returns(Task.CompletedTask);
+
+            _userManagerMock = new Mock<UserManager<User>>();
+
+            _userManagerMock.Setup(u => u.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync((string name) => UsersContextMock
+                    .Where(u => u.UserName == name)
+                    .First()
+                );
+                
+            Services = new ServiceCollection()
+                .AddAutoMapper(c => 
+                {
+                    c.AddProfile<IdentityUserMappingProfile>();
+                }, Assembly.GetExecutingAssembly())
+                .AddSingleton(_redisCacheRepositoryMock.Object)
+                .AddSingleton(_userManagerMock.Object)
+                .AddSingleton<IConnectionsManager>()
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ src/Server/Nocturne/Nocturne.InfrastructureTests1/Messaging/ConnectionsManagerTests.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Nocturne.Core.Managers;
+using Nocturne.Infrastructure.Security.Entities;
+using Nocturne.InfrastructureTests.Helpers;
+using NUnit.Framework;
+
+namespace Nocturne.Infrastructure.Messaging.Tests
+{
+    [TestFixture()]
+    public class ConnectionsManagerTests
+    {
+        public readonly ConnectionsManager<User> _connectionsManager;
+
+        public readonly UserManager<User> _userManager;
+
+        private readonly IMapper _mapper;
+
+        public ConnectionsManagerTests()
+        {
+            _connectionsManager = (ConnectionsManager<User>)
+                DependencyHelper.Services
+                .GetRequiredService<IConnectionsManager>();
+
+            _mapper = DependencyHelper.Services.GetRequiredService<IMapper>();
+        }
+
+        [Test()]
+        public void ConnectionsManagerTest()
+        {
+        }
+
+        [Test()]
+        public async Task ConnectTest()
+        {
+            var user = _mapper.Map<User, Core.Models.User>(DependencyHelper.UsersContextMock
+                .First());
+
+            await _connectionsManager.Connect(user, Guid.NewGuid().ToString());
+        }
+
+        [Test()]
+        public void DisconectTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void DisconectTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void GetUserConnectionsTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void IsConnectedUserTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
