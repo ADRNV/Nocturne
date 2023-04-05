da80db7db679befb25a234c80110fb41564baf85
NKT <49834917+ADRNV@users.noreply.github.com>
2023-04-06 01:29:16 +0300

    Add MediatR handlers for groups

---
+++ src/Server/Nocturne/Nocturne/Features/Groups/RemoveFromGroup.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Nocturne.Core.Managers;
+using Nocturne.Core.Models;
+using Nocturne.Models;
+
+namespace Nocturne.Features.Groups
+{
+    public class RemoveFromGroup
+    {
+        public record Command(string UserName, Group Group) : IRequest<bool>;
+
+        public class Handler : IRequestHandler<Command, bool>
+        {
+            private readonly IGroupManager _groupManager;
+
+            public Handler(IGroupManager groupManager)
+            {
+                _groupManager = groupManager;
+            }
+
+            public async Task<bool> Handle(Command request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    return await _groupManager.RemoveFromGroup(request.UserName, request.Group, cancellationToken);
+                }
+                catch
+                {
+                    throw new RestException(System.Net.HttpStatusCode.NotFound);
+                }
+            }
+        }
+    }
+}
