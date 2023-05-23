04a05959c18b535f3e3c1b5892507adc9bcde168
NKT <49834917+ADRNV@users.noreply.github.com>
2023-05-23 23:54:31 +0300

    Clean up

--- src/Client/nocturne-client/src/App.js
+++ src/Client/nocturne-client/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import SignInForm from './components/form/signin/SignInForm';
 
 function App() {
 
   return (
     <div className="App">
-        <SignInForm/>
+        
     </div>
   );
 }
