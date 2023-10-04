2c69e9279a1329ce0c27d6ce7d2ee53beee10dee
ADRN_NKT <combinerk@gmail.com>
2023-10-04 22:43:51 +0300

    Fix SignalR client

--- src/Server/Nocturne/Nocturne.Api.Client/Messaging/MessagingClientBase.cs
+++ src/Server/Nocturne/Nocturne.Api.Client/Messaging/MessagingClientBase.cs
@@ -1,13 +1,13 @@
 ﻿using Nocturne.Core.Clients;
-using Nocturne.Core.Models;
+using CoreMessage = Nocturne.Core.Models.Message;
 
 namespace Nocturne.Api.Client.Messaging
 {
     public abstract class MessagingClientBase : IMessagingClient
     {
-        public abstract Func<Message, string, bool> RecivedMessageCallback { get; set; }
+        public abstract Func<CoreMessage, string, bool> RecivedMessageCallback { get; set; }
 
-        public abstract Task<bool> SendMessage(Message message, string to);
+        public abstract Task<bool> SendMessage(CoreMessage message, string to);
 
         public abstract Task Start();
 
