d38f12386cd3a971a8919b2a6a578c0337ecedfc
ADRN_NKT <combinerk@gmail.com>
2024-10-20 12:53:27 +0300

    Add navigation, split modules

--- src/Client/nocturne-client-angular/nocturne-client/src/app/app.routes.ts
+++ src/Client/nocturne-client-angular/nocturne-client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './features/auth/auth/auth.component';
+import { NgModule } from '@angular/core';
+import { AboutComponent } from './features/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'about', pathMatch:'full'},
+    {path:'about', component: AboutComponent},
+    {path: 'auth', component: AuthComponent},
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+export class AppRoutingModule {
+  
+}
+  
