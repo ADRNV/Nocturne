853855e979ed91c09883ba2f3f6a9d4c6ec718a1
ADRN_NKT <combinerk@gmail.com>
2024-11-04 16:45:16 +0300

    Add RegistrationPAge in navigation

--- src/Client/nocturne-client-angular/nocturne-client/src/app/app.routes.ts
+++ src/Client/nocturne-client-angular/nocturne-client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AppComponent } from './app.component';
 import { AuthComponent } from './features/auth/auth/auth.component';
 import { NgModule } from '@angular/core';
 import { AboutComponent } from './features/about/about.component';
+import { RegisterUserComponent } from './features/register-user/register-user.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'about', pathMatch:'full'},
     {path:'about', component: AboutComponent},
     {path: 'auth', component: AuthComponent},
+    {path: 'register', component: RegisterUserComponent }
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
